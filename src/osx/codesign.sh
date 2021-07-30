5e5e0694d50ca11e12ff780f7274a873eaf57795
ta <ta@zzv.io>
2021-07-30 16:11:45 +0500

    Updated osx

--- src/osx/codesign.sh
+++ src/osx/codesign.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sw_vers
 mkdir -p src/osx/zzv.app/Contents/MacOS/
-cp target/gluonfx/x86_64-darwin/Zzv-view src/osx/zzv.app/Contents/MacOS/Zzv-view
+cp target/gluonfx/x86_64-darwin/semux-core src/osx/zzv.app/Contents/MacOS/Zzv-view
 cd src/osx
 sed -e 's/CFBundleVersionPlaceHolder/'$GITHUB_RUN_NUMBER'/g' zzv.app/Contents/Info.plist > zzv.app/Contents/Temp.plist
 mv zzv.app/Contents/Temp.plist zzv.app/Contents/Info.plist
