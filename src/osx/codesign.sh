ab4418e36ff06fa50fb25d22c0778e17776c37be
API user <api@zzv.io>
2021-08-03 10:27:46 +0000

    Added new version from pipeline

--- src/osx/codesign.sh
+++ src/osx/codesign.sh
@@ -3,7 +3,7 @@ sw_vers
 mkdir -p src/osx/zzv.app/Contents/MacOS/
 cp target/gluonfx/x86_64-darwin/semux-core src/osx/zzv.app/Contents/MacOS/Zzv-view
 cd src/osx
-
-sed -e 's/CFBundleVersionPlaceHolder/63/g' zzv.app/Contents/Info.plist > zzv.app/Contents/Temp.plist
+let "BUILD = $GITHUB_RUN_NUMBER + 30"
+sed -e 's/CFBundleVersionPlaceHolder/'$BUILD'/g' zzv.app/Contents/Info.plist > zzv.app/Contents/Temp.plist
 mv zzv.app/Contents/Temp.plist zzv.app/Contents/Info.plist
 codesign -s "$APP_CERT_NAME" -f --entitlements entitlement.plist zzv.app
